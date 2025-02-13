76f133bb4ff8062bbd414cf0f6c8988dec1ce5b4
Soung jin Ryu <neocat@kakao.com>
2025-02-13 12:11:29 +0900

    Fix.  인터페이스 종속 문제 수정

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
 'use strict';
-
 import mysql, { Pool, PoolConnection, RowDataPacket } from 'mysql2/promise';
 import { env } from 'process';
-import { ErrorLog, LogType, queryFunctionType, ResqultQuery, SqlResultParser } from './interface';
-import { Paging, SelectPagingResult } from './interface/page';
 
 // out interface
-export * from './interface';
-export * from './interface/page';
 
 export const format = mysql.format;
 
 // 커넥션 쿼리 함수
 // select / insert / update / delete
 const newLine = /\n/g;
+
+export interface sqlInsertUpdate {
+    affectedRows: number;
+    changedRows: number;
+    insertId: number;
+}
+
+export enum SQLType {
+    select = 'select',
+    insert = 'insert',
+    update = 'update',
+    delete = 'delete',
+}
+
+export enum LogType {
+    ALL = 'ALL',
+    SIMPLE = 'SIMPLE',
+    NONE = 'NONE',
+}
+
+// SQL 타입 - insert / update / delete 인 경우  queryFunctionType 의 리턴 타입이 sqlInsertUpdate
+export type SqlInsertUpdate = SQLType.insert | SQLType.update | SQLType.delete;
+
+export type ResqultQuery<E> = E extends SqlInsertUpdate ? sqlInsertUpdate : Array<E>;
+export type queryFunctionType = <E>(query: string, ...params: any[]) => Promise<ResqultQuery<E>>;
+
+export type SqlResultParser = (k: string, v: any) => any;
+export type ErrorLog = (query: string, params: any[]) => void;
 
 let limit = 10; // 기본 페이징 사이즈 (페이징 쿼리에 파라메타가 생략된 경우)
 let _log: LogType = LogType.ALL;
@@ -110,6 +133,24 @@ export const query = async <E>(pool: Pool, query: string, ...params: any[]): Pro
 
 export const selectOne = async <E>(pool: Pool, _query: string, ...params: any[]) =>
     query<E>(pool, _query, ...params).then(([row]: any) => (Array.isArray(row) ? row[0] : row));
+
+export interface Paging {
+    page: number;
+    limit?: number;
+}
+export type seleceQueryOption = {
+    query: string;
+    page?: number;
+    limit?: number;
+};
+
+export type SelectPagingResult<E> = {
+    total: number;
+    totalPage: number;
+    limit: number;
+    page: number;
+    list: E[];
+};
 
 /**
  *
